73fcf36aed16fd0831732383253f0500a992c66c
saraMir26 <mirz0014@algonquinlive.com>
2026-02-10 03:38:17 -0500

    update

--- app.py
+++ app.py
@@ -3,16 +3,22 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+port = int(os.environ.get("PORT", 3030))
+
 @app.route("/products", methods=["GET"])
 def get_products():
     return jsonify([
-        {"id": 1, "name": "Dog Food", "price": 19.99},
-        {"id": 2, "name": "Cat Food", "price": 34.99},
-        {"id": 3, "name": "Bird Seeds", "price": 10.99},
+        { "id": 1, "name": "Dog Food", "price": 19.99 },
+        { "id": 2, "name": "Cat Food", "price": 34.99 },
+        { "id": 3, "name": "Bird Seeds", "price": 10.99 },
     ])
 
-# DO NOT call app.run() in Azure
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
