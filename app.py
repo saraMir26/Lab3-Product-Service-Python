b4c89aebabb64dd9680b07b8a34e1f2178569c1d
saraMir26 <mirz0014@algonquinlive.com>
2026-02-10 23:49:44 -0500

    a

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+print(">>>> STARTING PRODUCT SERVICE <<<<")
+
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
 
-print("STARTING PRODUCT SERVICE")
-
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)  # allow all for now
+CORS(app)
 
 @app.route("/")
 def home():
@@ -13,11 +16,7 @@ def home():
 
 @app.route("/products", methods=["GET"])
 def get_products():
-    return jsonify([
-        { "id": 1, "name": "Dog Food", "price": 19.99 },
-        { "id": 2, "name": "Cat Food", "price": 34.99 },
-        { "id": 3, "name": "Bird Seeds", "price": 10.99 },
-    ])
+    return jsonify([...])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
