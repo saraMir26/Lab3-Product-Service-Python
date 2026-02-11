58c45d6cf34f50ffcec6c4e764ae06af610c82f0
saraMir26 <mirz0014@algonquinlive.com>
2026-02-11 00:52:33 -0500

    5

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 print("STARTING PRODUCT SERVICE")
 
@@ -19,4 +20,5 @@ def get_products():
     ])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
